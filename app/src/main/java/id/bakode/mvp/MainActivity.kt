e7add9168b68b9b85229b56003c37b60e005f794
A. A. Sumitro <aasumitro@gmail.com>
2019-02-13 22:14:10 +0800

    add buildSrc module

---
+++ app/src/main/java/id/bakode/mvp/MainActivity.kt
@@ -0,0 +1,23 @@
+package id.bakode.mvp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+/**
+ * Created by A. A. Sumitro on 2/13/2019
+ * aasumitro@merahputih.id
+ * https://aasumitro.id
+ */
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(
+            savedInstanceState: Bundle?
+    ) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
